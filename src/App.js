8ec1cc7e2d16aec40478077a386d320dc866fa50
Deee3 <dtran0418@gmail.com>
2022-11-03 19:31:35 -0400

    skeleton of reactmvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from'react-dom';
+//import { Provider } from'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//CssBaseLine will auto fix some minor styling
+//Grid is the layout grid that creates visual consistency between 
+//
+
+import { CssBaseline, Grid } from '@mui/material';
+
+
+
+import Header from './components/Header/Header.jsx';
+import List from './components/List/List.jsx';
+import Map from './components/Map/Map.jsx';
+import RestaurantDetails from './components/RestaurantDetails/RestaurantDetails.jsx';
+//Grid item xs={12}, grid will take full width on mobile devices
+// on md devices and larger will only take 4 spaces, the list was taking 
+//much less space than map
+//<Grid container spacing={3} style={{width: '100%'}}> inline styling means it needs 
+//to be in a object
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+
+            <Grid container spacing={3} style={{width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const RestaurantDetails = () => {
+
+    return (
+        <h1>RestaurantDetails</h1>
+
+
+    );
+
+}
+
+export default RestaurantDetails;
